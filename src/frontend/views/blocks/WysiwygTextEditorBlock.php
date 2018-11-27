79cba40957d21c247d62ca76fedcc66d2cb6c9e0
andrzej <andrzej@liberosoft.pl>
2018-11-27 14:02:30 +0100

    minor bugs

--- src/frontend/views/blocks/WysiwygTextEditorBlock.php
+++ src/frontend/views/blocks/WysiwygTextEditorBlock.php
@@ -10,22 +10,3 @@
  */
 ?>
 <?= $this->varValue('text'); ?>
-
-
-<select name="id_wydzial" size="1" class="formSelect" wtx-context="2F176E50-92EA-4FAA-88DD-99C3D1799E67">
-Wydział Promocji Sportu i Kultury
-Biuro Współpracy z Organizacjami Pozarządowymi
-SR
-O
-Wydział Organizacyjny
-MHE
-Szkoła Podstawowa nr 5 im. Marii Konopnickiej i Zespół Szkół Sportowychw Ełku
-USC
-Wydział Świadczeń
-WŚ
-E
-MP Światełko
-Polityka Społeczna
-Wydział Polityki Społecznej
-Biuro Prezydenta
-SiR
